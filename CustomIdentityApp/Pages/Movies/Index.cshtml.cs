5c54c67e12a9323f584464cd0275180f60b911fd
m1r0n0 <Oleksandr.Myroniuk@udelphi.com>
2022-11-15 14:57:29 +0200

    Tags in Movies works, but not css (why?)

--- CustomIdentityApp/Pages/Movies/Index.cshtml.cs
+++ CustomIdentityApp/Pages/Movies/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CustomIdentityApp.Models;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 
 namespace CustomIdentityApp.Pages.Movies
 {
