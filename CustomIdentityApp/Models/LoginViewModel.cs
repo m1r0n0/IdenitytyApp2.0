fe7dd08fde2f954c75bdf9eb5a2ee3f1868c8f2e
m1r0n0 <Oleksandr.Myroniuk@udelphi.com>
2022-11-15 15:48:13 +0200

    Added Login

---
+++ CustomIdentityApp/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomIdentityApp.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name ="Remember me?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; } = string.Empty;
+    }
+}
