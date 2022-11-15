bb41a1f4e6e21747e95aa86ab3f17b0da5d3754d
m1r0n0 <Oleksandr.Myroniuk@udelphi.com>
2022-11-15 14:43:42 +0200

    Movies asp tags don't work

---
+++ CustomIdentityApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CustomIdentityApp.Models;
+
+namespace CustomIdentityApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CustomIdentityApp.Models.ApplicationContext _context;
+
+        public IndexModel(CustomIdentityApp.Models.ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
